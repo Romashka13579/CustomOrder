550b07ffef09cd260f952fb20edc5efbdda7de89
Romashka <rroma13579@gmail.com>
2023-05-19 01:26:25 +0200

    adding unity 3d

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
-var orderCustomItem = document.querySelectorAll(".order-custom-item");
+// var orderCustomItem = document.querySelectorAll(".order-custom-item");
 
-orderCustomItem.forEach(item => {
-    item.addEventListener('click', ()=>{
-        if(item.querySelectorAll(".order-custom-list").style.marginTop == "0px"){
-            item.querySelectorAll(".order-custom-list").style.marginTop = "-200%";
-        }
-        else if(item.querySelectorAll(".order-custom-list").style.marginTop == "-200%")
-        {
-            item.querySelectorAll(".order-custom-list").style.marginTop = "0px";
-        }
-    })
-});
+// orderCustomItem.forEach(item => {
+//     item.addEventListener('click', ()=>{
+//         if(item.querySelectorAll(".order-custom-list").style.marginTop == "0px"){
+//             item.querySelectorAll(".order-custom-list").style.marginTop = "-200%";
+//         }
+//         else if(item.querySelectorAll(".order-custom-list").style.marginTop == "-200%")
+//         {
+//             item.querySelectorAll(".order-custom-list").style.marginTop = "0px";
+//         }
+//     })
+// });
